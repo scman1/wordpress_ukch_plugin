a2fdadea2010501e1d0a890bf7f1788ee034ebf1
Abraham Nieva de la Hidalga <a_nieva@hotmail.com>
2021-05-06 18:49:17 +0100

    changed the call to consume form rails api

--- wp-api-client.php
+++ wp-api-client.php
@@ -23,43 +23,25 @@ function get_articles_data($atts){
 	];
 	$atts = shortcode_atts($defaults, $atts, 'ukch_articles');
 	
-	$url = 'https://jsonplaceholder.typicode.com/users';
+	$params = array(
+		'page' => '20'
+	);	
 	
-    $arguments = array(
-        
-    );
-
-	$response = wp_remote_get( $url, $arguments );
-
-	if ( is_wp_error( $response ) ) {
-		$error_message = $response->get_error_message();
-		return "Something went wrong: $error_message";
-	} 
-	$results = json_decode( wp_remote_retrieve_body( $response ) );
+	$results = get_articles ('articles.json', $params);
 
 	$html = "";
 	$html = "<h2>" . $atts['title'] . "</h2>";
-	$html .= "<table>";
-	$html .= "<tr>";
-	$html .= "<td>title</td>";
-	$html .= "<td>doi</td>";
-	$html .= "<td>journal</td>";
-	$html .= "</tr>";
 	foreach ($results as $result){
-		$html .= "<tr>";
-		$html .= "<td>" . $result->id . "</td>";
-		$html .= "<td>" . $result->name . "</td>";
-		$html .= "<td>" . $result->email . "</td>";
-		$html .= "</tr>";
+		$html .= "<p>";
+		$html .= "<b>" . $result["title"] . "</b>, ";
+		$html .=  $result["container_title"] . ", ";
+		$html .=  $result["doi"] . ".";
+		$html .= "</p>";
 	}
-	$html .= "</table>";
 	return $html;
 }
 
-
-/*   
-
-function ukchapi_get_as_json( $action, $params ) {
+function get_articles( $action, $params ) {
  
     $api_endpoint = "http://188.166.149.246/";
  
@@ -89,92 +71,3 @@ function ukchapi_get_as_json( $action, $params ) {
  
     return $json_data;
 }
-
-function show_publications_list() {
-	 $params = array(
-				'page' => '1'
-			);	
-    $pubs_data = ukchapi_get_as_json('articles.json', $params); //decoded json data
-?>    
-    <div class="publications-list">
-        <ul>
-        <?php if (count($pubs_data) > 0) : ?>
- 
-            <?php foreach ($pubs_data as $item) : ?>  
-                <li>
-                    <?php echo $item["title"]; ?>, <?php echo $item["container_title"]; ?>,
-                    <?php echo $item["doi"]; ?>
-                </li>
-            <?php endforeach; ?>
- 
-        <?php else : ?>
-            Your shopping cart is empty.
-        <?php endif; ?>
-        </ul>
-    </div>
-<?php
-}
-
-// Creating the widget 
-class uac_widget extends WP_Widget {
-  
-function __construct() {
-parent::__construct(
-  
-// Base ID of your widget
-'uac_widget', 
-  
-// Widget name will appear in UI
-__('UKCH API Client Widget', 'uac_widget_domain'), 
-  
-// Widget description
-array( 'description' => __( 'Widget for querying UKCH rails API', 'uac_widget_domain' ), ) 
-);
-}
-  
-// Creating widget front-end
-  
-public function widget( $args, $instance ) {
-$title = apply_filters( 'widget_title', $instance['title'] );
-  
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-if ( ! empty( $title ) )
-echo $args['before_title'] . $title . $args['after_title'];
-show_publications_list();
-echo $args['after_widget'];
-}
-          
-// Widget Backend 
-public function form( $instance ) {
-if ( isset( $instance[ 'title' ] ) ) {
-$title = $instance[ 'title' ];
-}
-else {
-$title = __( 'New title', 'uac_widget_domain' );
-}
-// Widget admin form
-?>
-<p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-</p>
-<?php 
-}
-      
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
- 
-// Class wpb_widget ends here
-} 
- 
-// Register and load the widget
-function uac_load_widget() {
-    register_widget( 'uac_widget' );
-}
-add_action( 'widgets_init', 'uac_load_widget' );
- */
